cf916f10b2d42ba197e0393895ed67c532147683
notchyves <judah.richardsrogan@gmail.com>
2025-04-12 01:18:24 -0400

    feat: cross-platform game memory loader?

--- src/load/main.cpp
+++ src/load/main.cpp
@@ -1,6 +1,6 @@
 #include "main.hpp"
 
-void init() {
+void init(std::span<std::byte> bytes) {
 	logger::init();
 	logger::info("loaded selaura");
 }
@@ -9,15 +9,56 @@ void init() {
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
 	if (dwReason == DLL_PROCESS_ATTACH) {
-		if (GetModuleHandleA("Minecraft.Windows.exe") == nullptr) return FALSE;
+        HMODULE hMinecraft = GetModuleHandleA("Minecraft.Windows.exe");
+        if (hMinecraft == nullptr) return FALSE;
+
 		DisableThreadLibraryCalls(hModule);
 		try {
-            std::thread(init).detach();
+            MODULEINFO modInfo;
+            if (GetModuleInformation(GetCurrentProcess(), hMinecraft, &modInfo, sizeof(MODULEINFO))) {
+                std::span<std::byte> game_bytes(
+                    reinterpret_cast<std::byte*>(modInfo.lpBaseOfDll),
+                    modInfo.SizeOfImage
+                );
+
+                std::thread([=]() {
+                    init(game_bytes);
+                }).detach();
+            }
+            else {
+                return FALSE;
+            }
         } catch (...) {
             return FALSE;
         }
 	}
 
 	return TRUE;
+}
+#endif
+
+#ifdef PLATFORM_LINUX
+extern "C" [[gnu::visibility("default")]] void mod_preinit() {}
+
+extern "C" [[gnu::visibility("default")]] void mod_init() {
+    static std::span<std::byte> game_bytes;
+
+    dl_iterate_phdr([](dl_phdr_info* info, size_t, void* mc) {
+        auto h = dlopen(info->dlpi_name, RTLD_NOLOAD);
+        dlclose(h);
+        if (h == mc) {
+            for (auto& phdr : std::span{ info->dlpi_phdr, info->dlpi_phnum }) {
+                if (phdr.p_type == PT_LOAD && phdr.p_flags & PF_X) {
+                    game_bytes = { reinterpret_cast<std::byte*>(info->dlpi_addr + phdr.p_vaddr), phdr.p_memsz };
+                    return 1;
+                }
+            }
+        }
+        return 0;
+        }, dlopen("libminecraftpe.so", 0));
+
+    std::thread([=]() {
+        init(game_bytes);
+    }).detach();
 }
 #endif
--- src/load/main.hpp
+++ src/load/main.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
-#ifdef _WIN32
-#include <Windows.h>
+#if defined(__unix__) || defined(__APPLE__)
+#if !defined(__ANDROID__)
+#define PLATFORM_LINUX
+#endif
 #endif
 
 #include <thread>
+#include <span>
+#include <cstddef>
 #include "io/logger.hpp"
 
 #ifdef _WIN32
+#include <Windows.h>
+#include <Psapi.h>
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved);
+#endif
+
+#ifdef PLATFORM_LINUX
+extern "C" [[gnu::visibility("default")]] void mod_preinit();
+extern "C" [[gnu::visibility("default")]] void mod_init();
 #endif
