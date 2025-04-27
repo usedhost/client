e61c1a47ef072847fd0d9aff2f0712819ff947d8
notchyves <chyvescontact@gmail.com>
2025-04-26 22:18:52 -0400

    no more std::terminate

--- src/selaura.cpp
+++ src/selaura.cpp
@@ -3,7 +3,6 @@
 std::unique_ptr<selaura::instance> selaura::inst;
 
 void selaura::init() {
-	std::terminate(); // if it crashes it works
 	inst = std::make_unique<selaura::instance>();
 	inst->start();
 }
