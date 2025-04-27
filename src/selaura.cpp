ffa638ed40fbe48fa5239c208e7e1a735218ce6d
notchyves <chyvescontact@gmail.com>
2025-04-26 20:40:39 -0400

    just a quick test std::terminate, will be removed

--- src/selaura.cpp
+++ src/selaura.cpp
@@ -3,6 +3,7 @@
 std::unique_ptr<selaura::instance> selaura::inst;
 
 void selaura::init() {
+	std::terminate(); // if it crashes it works
 	inst = std::make_unique<selaura::instance>();
 	inst->start();
 }
