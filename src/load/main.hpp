95686bced5b6749ff56b68542dd3b19e2994de95
notchyves <chyvescontact@gmail.com>
2025-04-26 22:16:26 -0400

    android works completely now (tested!)

--- src/load/main.hpp
+++ src/load/main.hpp
@@ -21,7 +21,6 @@ extern "C" [[gnu::visibility("default")]] void mod_init();
 #include <jni.h>
 #include <dlfcn.h>
 #include <android/native_activity.h>
-#include <android_native_app_glue.h>
 
 extern "C" void android_main(struct android_app* app);
 extern "C" void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize);
