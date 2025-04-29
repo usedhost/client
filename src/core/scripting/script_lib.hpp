2d436747fcece8c478d8c6edb1f40ddff9e5a25a
notchyves <chyvescontact@gmail.com>
2025-04-29 14:08:10 -0400

    static + dynamic script lib & advice taken from brady

--- src/core/scripting/script_lib.hpp
+++ src/core/scripting/script_lib.hpp
@@ -1,18 +1,35 @@
 #pragma once
+
 #include <LuaBridge/LuaBridge.h>
+#include <concepts>
+#include <type_traits>
 
 namespace selaura::core {
+	template <typename T>
+	concept StaticScriptLib = requires(lua_State * L) {
+		{ T::init(L) } -> std::same_as<void>;
+	};
+
+	template <typename T>
+	concept DynamicScriptLib = requires(T t, lua_State * L) {
+		{ t.init(L) } -> std::same_as<void>;
+	};
+
 	class script_lib {
 	public:
 		script_lib() = default;
 		virtual ~script_lib() = default;
 
-		virtual void init(lua_state* L) {};
+		virtual void init(lua_State* L) = 0;
 
-		template <typename T>
-		static void register_lib(lua_state* L) {
-			T lib;
-			lib.init(L);
+		template <StaticScriptLib T>
+		static void register_lib(lua_State* L) {
+			T::init(L);
+		}
+
+		template <DynamicScriptLib T>
+		static void register_lib(lua_State* L) {
+			T{}.init(L);
 		}
 	};
 };
