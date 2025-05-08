0b7262ad26371b04da51bf52224244fd65a09c80
notchyves <chyvescontact@gmail.com>
2025-05-08 13:35:13 -0400

    clarification!

--- src/client/io/io.hpp
+++ src/client/io/io.hpp
@@ -24,7 +24,7 @@ namespace selaura {
 			}
 #elif defined(SELAURA_LINUX)
 			data_folder = "/data/data/com.mojang.minecraftpe/Selaura";
-#else
+#elif defined(SELAURA_ANDROID)
 			data_folder = "/data/data/com.selauraclient.launcher/Selaura";
 #endif
 			log_file = data_folder / "logs.txt";
