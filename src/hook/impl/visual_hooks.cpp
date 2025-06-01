b794173d3d7ba9737e5fffef33588b5a278203c0
chyves <chyvescontact@gmail.com>
2025-05-31 20:37:10 -0400

    remove splash text for a test

--- src/hook/impl/visual_hooks.cpp
+++ src/hook/impl/visual_hooks.cpp
@@ -2,7 +2,7 @@
 
 namespace selaura {
 	visual_hooks::visual_hooks(hook_manager& mgr) : hook_group(mgr) {
-        mgr.register_hook(selaura::signatures::splashtextrenderer_loadsplashes, [](void* ofunc, void* a1, void* a2, void* a3, void* a4) {
+        /*mgr.register_hook(selaura::signatures::splashtextrenderer_loadsplashes, [](void* ofunc, void* a1, void* a2, void* a3, void* a4) {
             using ofunc_t = selaura::signatures::splashtextrenderer_loadsplashes_t;
             std::vector<std::string>* result = std::bit_cast<ofunc_t>(ofunc)(a1, a2, a3, a4);
 
@@ -12,6 +12,6 @@ namespace selaura {
             }
 
             return result;
-        });
+        });*/
 	};
 }
